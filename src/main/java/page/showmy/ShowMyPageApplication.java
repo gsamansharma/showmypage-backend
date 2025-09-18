9e9ba4631fafb69f67952a27bd29aad65b1ef79f
gsamansharma <gsamansharma@gmail.com>
2025-09-18 21:10:00 +0530

    config: Add env-based Spring Boot setup

--- src/main/java/page/showmy/ShowMyPageApplication.java
+++ src/main/java/page/showmy/ShowMyPageApplication.java
@@ -1,8 +1,13 @@
 package page.showmy;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ShowMyPageApplication {
 
     public static void main(String[] args) {
+        SpringApplication.run(ShowMyPageApplication.class, args);
     }
 
 }
--- src/test/java/page/showmy/ShowMyPageApplicationTests.java
+++ src/test/java/page/showmy/ShowMyPageApplicationTests.java
@@ -1,7 +1,9 @@
 package page.showmy;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class ShowMyPageApplicationTests {
 
     @Test
