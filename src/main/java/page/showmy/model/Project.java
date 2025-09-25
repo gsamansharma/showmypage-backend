4eaa994fe910e6d1dace3021a46cbf117a73dbd5
gsamansharma <gsamansharma@gmail.com>
2025-09-25 13:55:00 +0530

    refactor(model): Update Project entity with description, videoUrl, and imageUrls

--- src/main/java/page/showmy/model/Project.java
+++ src/main/java/page/showmy/model/Project.java
@@ -13,20 +13,25 @@ public class Project {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
-    @Column(length = 1024)
+
+    @Lob
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String description;
+
     private String icon;
     private String liveUrl;
     private String githubUrl;
+    private String videoUrl;
 
     @ElementCollection(fetch = FetchType.EAGER)
-    @CollectionTable(name = "project_tags", joinColumns = @JoinColumn(name = "project_id"))
-    @Column(name = "tag")
-    private List<String> tags;
+    @CollectionTable(name = "project_images", joinColumns = @JoinColumn(name = "project_id"))
+    @Column(name = "image_url")
+    private List<String> imageUrls;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="user_id")
+    @JoinColumn(name="user_id", nullable = false)
     @JsonIgnore
     private User user;
 }
