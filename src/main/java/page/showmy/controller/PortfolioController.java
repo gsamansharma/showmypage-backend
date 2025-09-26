46718efc7911bc4313b99139ab95caf98e0769f5
gsamansharma <gsamansharma@gmail.com>
2025-09-26 09:50:00 +0530

    refactor(controller): Simplify PortfolioController to use service and DTO

--- src/main/java/page/showmy/controller/PortfolioController.java
+++ src/main/java/page/showmy/controller/PortfolioController.java
@@ -2,11 +2,9 @@ package page.showmy.controller;
 
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
-import page.showmy.model.*;
+import page.showmy.auth.dto.PortfolioDTO;
 import page.showmy.service.PortfolioService;
-import java.util.List;
 
 @Controller
 public class PortfolioController {
@@ -18,28 +16,7 @@ public class PortfolioController {
     }
 
     @QueryMapping
-    public User portfolio(@Argument String username) {
+    public PortfolioDTO portfolio(@Argument String username) {
         return portfolioService.getPortfolioByUsername(username);
-    }
-
-
-    @SchemaMapping(typeName = "Portfolio", field = "user")
-    public User getUserInfo(User user) {
-        return user;
-    }
-
-    @SchemaMapping(typeName = "Portfolio", field = "apps")
-    public List<Project> getApps(User user) {
-        return user.getProjects();
-    }
-
-    @SchemaMapping(typeName = "Portfolio", field = "publicationsData")
-    public List<Publication> getPublicationsData(User user) {
-        return user.getPublications();
-    }
-
-    @SchemaMapping(typeName = "Portfolio", field = "skillsData")
-    public List<SkillsCategory> getSkillsData(User user) {
-        return user.getSkillsCategories();
     }
 }
