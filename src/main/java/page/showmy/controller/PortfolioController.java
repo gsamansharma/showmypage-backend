8b9ee91737f1eaafee45c15427797352d6d152bd
gsamansharma <gsamansharma@gmail.com>
2025-09-26 18:04:00 +0530

    feat(portfolio): Expose GraphQL query 'users' to fetch all user profiles

--- src/main/java/page/showmy/controller/PortfolioController.java
+++ src/main/java/page/showmy/controller/PortfolioController.java
@@ -3,8 +3,11 @@ package page.showmy.controller;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
-import page.showmy.auth.dto.PortfolioDTO;
+import page.showmy.dto.PortfolioDTO;
+import page.showmy.dto.UserProfileDTO;
 import page.showmy.service.PortfolioService;
+
+import java.util.List;
 
 @Controller
 public class PortfolioController {
@@ -18,5 +21,10 @@ public class PortfolioController {
     @QueryMapping
     public PortfolioDTO portfolio(@Argument String username) {
         return portfolioService.getPortfolioByUsername(username);
+    }
+
+    @QueryMapping
+    public List<UserProfileDTO> users(){
+        return portfolioService.getAllUsers();
     }
 }
