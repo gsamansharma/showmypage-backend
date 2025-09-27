4444ab044578e6e51b191db97cd175a368a8f7f4
gsamansharma <gsamansharma@gmail.com>
2025-09-28 04:42:22 +0530

    feat(api): Implement all GraphQL mutations for portfolio controller

--- src/main/java/page/showmy/controller/PortfolioController.java
+++ src/main/java/page/showmy/controller/PortfolioController.java
@@ -1,13 +1,18 @@
 package page.showmy.controller;
 
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
-import page.showmy.dto.PortfolioDTO;
-import page.showmy.dto.UserProfileDTO;
+import page.showmy.dto.*;
+import page.showmy.model.Project;
+import page.showmy.model.Publication;
+import page.showmy.model.Skill;
 import page.showmy.service.PortfolioService;
 
+import java.security.Principal;
 import java.util.List;
+import java.util.Set;
 
 @Controller
 public class PortfolioController {
@@ -31,5 +36,53 @@ public class PortfolioController {
     @QueryMapping
     public UserProfileDTO user(@Argument String username) {
         return portfolioService.getUserProfileByUsername(username);
+    }
+
+    @MutationMapping
+    public UserProfileDTO updateUserProfile(@Argument UserProfileInputDTO profileInput, Principal principal) {
+        String username = principal.getName();
+        return portfolioService.updateUserProfile(username, profileInput);
+    }
+
+    @MutationMapping
+    public Project addProject(@Argument ProjectInputDTO projectInput, Principal principal) {
+        String username = principal.getName();
+        return portfolioService.addProject(username, projectInput);
+    }
+
+    @MutationMapping
+    public Project updateProject(@Argument Long projectId, ProjectInputDTO projectInput, Principal principal) {
+        String username = principal.getName();
+        return portfolioService.updateProject(projectId, username, projectInput);
+    }
+
+    @MutationMapping
+    public boolean deleteProject(@Argument Long projectId, Principal principal) {
+        String username = principal.getName();
+        return portfolioService.deleteProject(projectId, username);
+    }
+
+    @MutationMapping
+    public Publication addPublication(@Argument PublicationInputDTO publicationInput, Principal principal) {
+        String username = principal.getName();
+        return portfolioService.addPublication(username, publicationInput);
+    }
+
+    @MutationMapping
+    public Publication updatePublication(@Argument Long publicationId, PublicationInputDTO publicationInput, Principal principal) {
+        String username = principal.getName();
+        return portfolioService.updatePublication(publicationId, username, publicationInput);
+    }
+
+    @MutationMapping
+    public boolean deletePublication(@Argument Long publicationId, Principal principal) {
+        String username = principal.getName();
+        return portfolioService.deletePublication(publicationId, username);
+    }
+
+    @MutationMapping
+    public Set<Skill> updateUserSkills(@Argument List<Long> skillIds, Principal principal){
+        String username = principal.getName();
+        return portfolioService.updateUserSkills(username, skillIds);
     }
 }
