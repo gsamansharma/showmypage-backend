805b6c0a1692f60cfd5c7a903c9592508e89ac1c
gsamansharma <gsamansharma@gmail.com>
2025-09-28 04:37:26 +0530

    feat(validation): Add @NotBlank and @URL annotations to UserProfileInputDTO

--- src/main/java/page/showmy/dto/UserProfileInputDTO.java
+++ src/main/java/page/showmy/dto/UserProfileInputDTO.java
@@ -1,13 +1,31 @@
 package page.showmy.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.hibernate.validator.constraints.URL;
 
 @Data
 public class UserProfileInputDTO {
+    @NotBlank
     private String name;
+
+    @NotBlank
     private String title;
+
+    @NotBlank
+    @URL(message = "Resume url must a valid url")
     private String resumeUrl;
+
+    @NotBlank
+    @URL(message = "Profile Photo Link must be a valid url")
     private String profilePhoto;
+
+    @NotBlank
+    @URL(message = "Github account must be a valid url")
     private String github;
+
+    @NotBlank
+    @URL(message = "Linkedin account must be a valid url")
     private String linkedin;
 }
