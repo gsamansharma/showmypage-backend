e8a519c9eb017135bfa93d8536c222479e0afa43
gsamansharma <gsamansharma@gmail.com>
2025-09-28 05:38:00 +0530

    feat: Add @NotBlank annotations to PublicationInputDTO

--- src/main/java/page/showmy/dto/PublicationInputDTO.java
+++ src/main/java/page/showmy/dto/PublicationInputDTO.java
@@ -1,9 +1,11 @@
 package page.showmy.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class PublicationInputDTO {
+    @NotBlank
     private String title;
     private String description;
     private String url;
