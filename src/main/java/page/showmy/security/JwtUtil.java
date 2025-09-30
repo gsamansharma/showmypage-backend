4d5856e7c921047f55221eded41a597c233a6a3c
gsamansharma <gsamansharma@gmail.com>
2025-09-30 12:16:00 +0530

    refactor(jwt): Remove generateToken(email) overload

--- src/main/java/page/showmy/security/JwtUtil.java
+++ src/main/java/page/showmy/security/JwtUtil.java
@@ -60,10 +60,6 @@ public class JwtUtil {
         return createToken(claims, userDetails.getUsername());
     }
 
-    public String generateToken(String email) {
-        Map<String, Object> claims = new HashMap<>();
-        return createToken(claims, email);
-    }
 
     private String createToken(Map<String, Object> claims, String subject) {
         return Jwts.builder()
